65115ba84d65deb86182284ab61ead63253c7881
Nate Ferrero <nateferrero@gmail.com>
2012-09-30 22:19:03 +0000

    Removed bad idea run file

--- init/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/nate/software/nateferrero/gallery/venv
-./bin/activate
-sudo /home/nate/software/nateferrero/gallery/venv/bin/uwsgi --master --emperor /etc/uwsgi/apps-enabled --die-on-term --uid www-data --gid www-data --logto /var/log/uwsgi/emperor.log &;sudo chmod 777 /var/www/run/nateferrero.gallery.sock;sudo chown www-data:www-data /var/www/run/nateferrero.gallery.sock
